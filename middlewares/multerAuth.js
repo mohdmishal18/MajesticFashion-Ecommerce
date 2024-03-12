ce257417e5a0c9a0eb4339a660f8f9c4b17a403d
Mohammed Mishal <mohdmishal18@gmail.com>
2024-03-12 16:11:34 +0530

    multer fixing

--- middlewares/multerAuth.js
+++ middlewares/multerAuth.js
@@ -1,33 +1,22 @@
 const multer = require('multer');
 const path = require('path');
 
-// const storage = multer.diskStorage({
-//     destination : function(req,file,cb)
-//     { console.log('in multerrr')
-//         cb(null,path.join(__dirname,'../public/assets/img/productImage/original'),function(err,success)
-//         {   console.log('in multer',success);
-//             if(err)
-//             {   
-//                 console.log(err);
-//                 throw err;
-//             }
-//         });
-//     },
-//     filename : function (req,file,cb)
-//     {
-//         const name = Date.now() + '-' + file.originalname;
-
-//         cb(null, name);
-//     }
-// });
-
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        console.log('in multerrr');
-        cb(null, path.join(__dirname, '../public/assets/img/productImage/original'));
+    destination : function(req,file,cb)
+    { console.log('in multerrr')
+        cb(null,path.join(__dirname,'../public/assets/img/ProductImage/original'),function(err,success)
+        {   console.log('in multer',success);
+            if(err)
+            {   
+                console.log(err);
+                throw err;
+            }
+        });
     },
-    filename: function (req, file, cb) {
+    filename : function (req,file,cb)
+    {
         const name = Date.now() + '-' + file.originalname;
+
         cb(null, name);
     }
 });
